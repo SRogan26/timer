0fac2a80d8bc837aa8c128f67cbc4df01ec30abf
stephen rogan <srogan26@gmail.com>
2022-07-19 23:26:42 -0500

    added start/pause functionality and fixed bugs with countdown and 0 initial time input

--- assets/script.js
+++ assets/script.js
@@ -2,45 +2,87 @@
 const userHours = document.querySelector("#hoursInput");
 const userMinutes = document.querySelector("#minInput");
 const userSeconds = document.querySelector("#secInput");
-//Using embedded for loops for hours:minutes:seconds
 const hrDisplay = document.querySelector('#hrDisplay');
 const minDisplay = document.querySelector('#minDisplay');
 const secDisplay = document.querySelector('#secDisplay');
 const startBtn = document.querySelector('#startBtn');
 const resetBtn = document.querySelector('#resetBtn');
+const inputArea = document.getElementById('inputs');
 
 let timerId;
-startBtn.addEventListener('click', function () {
+let totalTime = 0;
+
+let hrInput = userHours.value * 1;
+let minInput = userMinutes.value * 1;
+let secInput = userSeconds.value * 1;
+//updates time display
+function updateTimeDisplay(hours, minutes, seconds) {
+    hrDisplay.innerText = hours;
+    minDisplay.innerText = minutes < 10 ? '0' + minutes : minutes;
+    secDisplay.innerText = seconds < 10 ? '0' + seconds : seconds;
+}
+//Update timer display to show input values on initial start of countdown
+function setInitialDisplay(){
+    hrInput = userHours.value * 1;
+    minInput = userMinutes.value * 1;
+    secInput = userSeconds.value * 1;
     if (!timerId) {
-        hrDisplay.innerText = userHours.value;
-        minDisplay.innerText = userMinutes.value * 1 < 10 ? '0' + userMinutes.value : userMinutes.value;
-        secDisplay.innerText = userSeconds.value * 1 < 10 ? '0' + userSeconds.value : userSeconds.value;
-        let totalTime = (userHours.value * 60 * 60) + (userMinutes.value * 60) + userSeconds.value * 1;
-        function updateCountdown() {
-            const hours = Math.floor(totalTime / 60 / 60);
-            let minutes = Math.floor(totalTime / 60 % 60);
-            let seconds = totalTime % 60;
+        updateTimeDisplay(hrInput, minInput, secInput);
+    }
+}
+//function to countdown and update timer display
+function updateCountdown() {
+    if(totalTime === 0) return;
+    totalTime--
 
-            totalTime--
+    const hours = Math.floor(totalTime / 60 / 60);
+    let minutes = Math.floor(totalTime / 60 % 60);
+    let seconds = totalTime % 60;
 
-            hrDisplay.innerText = hours;
-            minDisplay.innerText = minutes < 10 ? '0' + minutes : minutes;
-            secDisplay.innerText = seconds < 10 ? '0' + seconds : seconds;
+    updateTimeDisplay(hours, minutes, seconds);
+    //reset timerId and start button inner text when time runs out
+    if (totalTime === 0) {
+        clearInterval(timerId);
+        timerId = null;
+        startBtn.innerHTML = 'Start';
+    };
+}
 
-            console.log(totalTime);
-            if (totalTime < 0) {
-                clearInterval(timerId);
-                timerId = null;
-            };
-        }
-        updateCountdown();
+//Event listeners to update display when changing time entered
+userHours.addEventListener('input', () => {
+    setInitialDisplay();
+})
+userMinutes.addEventListener('input', () => {
+    setInitialDisplay();
+})
+userSeconds.addEventListener('input', () => {
+    setInitialDisplay();
+})
+
+//Click start/pause button
+startBtn.addEventListener('click', function () {  
+    //if initial timer start
+    if (!timerId) {
+        totalTime = (hrInput * 60 * 60) + (minInput * 60) + secInput * 1;
+        if(totalTime === 0) return;
+        startBtn.innerText = 'Pause';
+        setInitialDisplay();
         timerId = setInterval(updateCountdown, 999);
+    //if timer was running and is currently paused
+    }else if(startBtn.innerText === 'Start'){
+        startBtn.innerText = 'Pause';
+        timerId = setInterval(updateCountdown, 999);
+    //if timer is currently running and you want to pause
+    }else {
+        startBtn.innerText = 'Start';
+        clearInterval(timerId);
+        timerId = -1;
     }
 })
+//Click Reset Button
 resetBtn.addEventListener('click', function () {
-    hrDisplay.innerText = userHours.value;
-    minDisplay.innerText = userMinutes.value * 1 < 10 ? '0' + userMinutes.value : userMinutes.value;
-    secDisplay.innerText = userSeconds.value * 1 < 10 ? '0' + userSeconds.value : userSeconds.value;
     clearInterval(timerId);
     timerId = null;
+    startBtn.innerHTML = 'Start';
+    setInitialDisplay();
 })
